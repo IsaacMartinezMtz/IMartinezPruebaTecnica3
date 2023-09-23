4952d8199fa98d67d8aba47542c019e7f90dde79
IsaacMartinez <isaamarti89@gmail.com>
2023-09-23 02:11:14 -0600

    Agregar archivos de proyecto.

---
+++ BL/Disco.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Disco
+    {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using(DL.IMartinezPruebaTecnica3Entities contex = new DL.IMartinezPruebaTecnica3Entities())
+                {
+                    var query = contex.GetAll().ToList();
+                    if (query !=  null) 
+                    {
+                        result.Objects = new List<object>();
+                        foreach (var obj in query)
+                        {
+                            ML.Disco discog = new ML.Disco();
+                            discog.IdDisco = obj.IdDisco;
+                            discog.Titulo = obj.Titulo;
+                            discog.Artista = obj.Artista;
+                            discog.GeneroMusical = obj.GeneroMusical;
+                            discog.Duracion = obj.Duracion;
+                            discog.Año = obj.Año;
+                            discog.Distribuidora = obj.Distribuidora;
+                            discog.Ventas = (int)obj.Ventas;
+                            discog.Disponibilidad = (bool)obj.Disponibilidad;
+
+                            result.Objects.Add(discog);
+                        }
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result Add(ML.Disco disco)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using(DL.IMartinezPruebaTecnica3Entities context = new DL.IMartinezPruebaTecnica3Entities())
+                {
+                    var query = context.DiscoAdd(disco.Titulo, disco.Artista, disco.GeneroMusical, disco.Duracion, disco.Año, disco.Distribuidora, disco.Ventas, disco.Disponibilidad);
+                    if (query > 0) 
+                    {
+                        result.Correct = true;
+                    }
+                    else 
+                    { 
+                        result.Correct = false; 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+        public static ML.Result Delete(ML.Disco disco)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.IMartinezPruebaTecnica3Entities context = new DL.IMartinezPruebaTecnica3Entities())
+                {
+                    var query = context.DiscoDelete(disco.IdDisco);
+                    if (query > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct= false;
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+        public static ML.Result GetById(int IdUsuario)
+        {
+            ML.Result result =new ML.Result();
+            try
+            {
+                using(DL.IMartinezPruebaTecnica3Entities context = new DL.IMartinezPruebaTecnica3Entities())
+                {
+                    var query = context.GetById(IdUsuario).FirstOrDefault();
+                    result.Object = new List<object>();
+                    if(query != null)
+                    {
+                        ML.Disco disco = new ML.Disco();
+                        disco.IdDisco = query.IdDisco;
+                        disco.Titulo = query.Titulo;
+                        disco.Artista = query.Artista;
+                        disco.GeneroMusical = query.GeneroMusical;
+                        disco.Duracion = query.Duracion;
+                        disco.Año = query.Año;
+                        disco.Distribuidora = query.Distribuidora;
+                        disco.Ventas = (int)query.Ventas;
+                        disco.Disponibilidad = (bool)query.Disponibilidad;
+
+                        result.Object=disco;
+                        result.Correct = true;
+
+                    }
+                    else
+                    {
+                        result.Correct=false;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+
+        }
+        public static ML.Result Update(ML.Disco disco)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.IMartinezPruebaTecnica3Entities context = new DL.IMartinezPruebaTecnica3Entities())
+                {
+                    var query = context.DiscoUpdate(disco.IdDisco, disco.Titulo, disco.Artista, disco.GeneroMusical, disco.Duracion, disco.Año, disco.Distribuidora, disco.Ventas, disco.Disponibilidad);
+                    if(query > 0)
+                    {
+                        result.Correct = true;
+
+                    }
+                    else
+                    {
+                        result.Correct=false;
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+    }
+}
---
+++ PL/Controllers/DiscoController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class DiscoController : Controller
+    {
+        // GET: Disco
+        public ActionResult GetAll()
+        {
+            ML.Disco disco = new ML.Disco();
+            ML.Result result = BL.Disco.GetAll();
+            if (result.Correct)
+            {
+                disco.Discos = result.Objects;
+            }
+            else
+            {
+                return View();
+            }
+            return View(disco);
+        }
+        [HttpGet]
+        public ActionResult Form(int? IdDisco)
+        {
+            ML.Disco disco = new ML.Disco();
+            if (IdDisco != null)
+            {
+                ML.Result result = BL.Disco.GetById(IdDisco.Value);
+                if (result.Correct)
+                {
+                    disco = (ML.Disco)result.Object;
+                }
+                return View(disco);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Form(ML.Disco disco)
+        {
+            if (disco.IdDisco == 0 || disco.IdDisco == null)
+            {
+
+
+                ML.Result result = BL.Disco.Add(disco);
+                if (result.Correct)
+                {
+                    ViewBag.Mensaje = "El disco se registro correctamente";
+                }
+                else
+                {
+                    ViewBag.Mensaje = "Error al intentar registrar";
+                }
+               
+            }
+            else
+            {
+                ML.Result result = BL.Disco.Update(disco);
+                if (result.Correct)
+                {
+                    ViewBag.Mensaje = "El disco se actualizo correctamente";
+                }
+                else
+                {
+                    ViewBag.Mensaje = "Error al actualizar el disco";
+                }
+
+            }
+            return PartialView("Modal");
+        }
+        [HttpGet]
+        public ActionResult Delete(ML.Disco disco)
+        {
+            ML.Result result = BL.Disco.Delete(disco);
+            if (result.Correct)
+            {
+                ViewBag.Mensaje = "Se ha eliminado correctamente el disco";
+            }
+            else
+            {
+                ViewBag.Mensaje = "Error al eliminar el disco";
+            }
+            return PartialView("Modal");
+        }
+    }
+}
